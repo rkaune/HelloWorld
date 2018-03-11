c7573367b253611aca8880902f3d065c6ab48117
richardkaune <richardkaune@10.254.186.85>
2018-03-11 13:15:12 -0400

    new commit

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -15,7 +15,7 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("HELLO WORLD");
+        System.out.println("Hello, 世界");
         // TODO code application logic here
     }
     
